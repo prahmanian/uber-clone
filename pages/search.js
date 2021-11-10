42e28dceb4c1283e7d4493f8db5bb66844485e5b
Pedram Rahmanian <pedram@gutenmade.com>
2021-11-09 17:12:41 -0800

    added back button to search page

--- pages/search.js
+++ pages/search.js
@@ -1,12 +1,36 @@
 import React from 'react'
 import tw from "tailwind-styled-components"
+import {IoArrowBack} from 'react-icons/io5'
+import Link from 'next/link'
 
 const Search = () => {
     return (
-        <div>
-            Search
-        </div>
+        <SearchWrapper>
+
+            {/* Back Button for Navigation */}
+            <Container>
+                <Link href='/' passHref>
+                    <BackButton />
+                </Link>
+            </Container>
+
+            
+            
+            
+        </SearchWrapper>
     )
 }
 
 export default Search
+
+const SearchWrapper = tw.div`
+    bg-gray-200 h-screen
+`
+
+const Container = tw.div`
+    bg-white p-2
+`
+
+const BackButton = tw(IoArrowBack)`
+    text-4xl ml-2 cursor-pointer
+`
