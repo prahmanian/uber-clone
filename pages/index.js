6d69f85f64eef40e948763aa5cd8aef17884d824
Pedram Rahmanian <pedram@gutenmade.com>
2021-11-09 16:40:53 -0800

    Added buttons to dashboard

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,9 @@ import tw from "tailwind-styled-components"
 import Map from './components/Map'
 import { useReducer } from 'react'
 import {SiUber} from 'react-icons/si'
+import {IoCarSportSharp} from 'react-icons/io5'
+import {MdElectricBike} from 'react-icons/md'
+import {BsFillCalendarPlusFill} from 'react-icons/bs'
 import User from './components/User'
 
 
@@ -25,7 +28,21 @@ export default function Home() {
           <User name="Pedram Rahmanian" image={"https://scontent-sea1-1.xx.fbcdn.net/v/t1.6435-9/68574398_10157595430107458_1831896932922949632_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=Qt9TteTJvRgAX96_mGj&_nc_ht=scontent-sea1-1.xx&oh=2ed2142e9c4a9bf3413e8fcad5b07c16&oe=61B063D2"} />
         </Header>
 
-        
+        {/* This portion of the dashboard contains our three buttons. */}
+        <DashboardButtons>
+          <DashboardBtn>
+            <Car />
+            <span>Ride</span>
+          </DashboardBtn>
+          <DashboardBtn>
+            <Bike />
+            <span>2-Wheels</span>
+          </DashboardBtn>
+          <DashboardBtn>
+            <Calendar/>
+            <span>Reserve</span>
+          </DashboardBtn>
+        </DashboardButtons>
 
 
       </Dashboard>
@@ -38,7 +55,7 @@ const Container = tw.div`
 `
 
 const Dashboard = tw.div`
-  bg-green-200 flex-1 p-4
+  flex-1 p-4
 `
 
 const Header = tw.div`
@@ -47,5 +64,24 @@ const Header = tw.div`
 
 const Logo = tw(SiUber)`
   text-8xl
+`
+
+const DashboardButtons = tw.div`
+  flex justify-between
+`
+
+const DashboardBtn = tw.div`
+  flex  flex-col flex-1 bg-gray-200 p-2 items-center justify-center m-2 h-32 rounded-lg transform hover:scale-105 transition text-xl &span:mt-40
+`
+
+const Car = tw(IoCarSportSharp)`
+  text-5xl text-gray-900 mb-1
+`
+
+const Bike = tw(MdElectricBike)`
+  text-5xl text-gray-900 mb-1
+`
+const Calendar = tw(BsFillCalendarPlusFill)`
+  text-5xl text-gray-900 mb-1
 `
 
