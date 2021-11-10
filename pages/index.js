e62bd809f56266563a161224a0ab686f001b94e3
Pedram Rahmanian <pedram@gutenmade.com>
2021-11-09 16:00:22 -0800

    Set the header portion of the dashboard.

--- pages/index.js
+++ pages/index.js
@@ -7,18 +7,27 @@ import { useReducer } from 'react'
 import {SiUber} from 'react-icons/si'
 import User from './components/User'
 
+
+
+
 export default function Home() {
 
   return (
     <Container>
+
       <Map />
+
       <Dashboard>
+
+        {/* The header of our dashboard shows the app logo and the name and image of the user. */}
         <Header>
-          <Logo><SiUber /></Logo>
+          <Logo />
           <User name="Pedram Rahmanian" image={"https://scontent-sea1-1.xx.fbcdn.net/v/t1.6435-9/68574398_10157595430107458_1831896932922949632_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=Qt9TteTJvRgAX96_mGj&_nc_ht=scontent-sea1-1.xx&oh=2ed2142e9c4a9bf3413e8fcad5b07c16&oe=61B063D2"} />
-
-
         </Header>
+
+        
+
+
       </Dashboard>
     </Container>
   )
@@ -36,7 +45,7 @@ const Header = tw.div`
   flex justify-between items-center
 `
 
-const Logo = tw.div`
-  large
+const Logo = tw(SiUber)`
+  text-8xl
 `
 
